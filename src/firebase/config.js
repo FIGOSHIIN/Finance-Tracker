da4d9c68af730676f160dc40aa24b9dfce6be267
Iliass Badou <97060220+FIGOSHIIN@users.noreply.github.com>
2025-07-07 13:15:21 +0100

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,12 +3,12 @@ import { getFirestore, Timestamp } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBXIwGvfEZleBM-CS2xifc6z8JzNP8fqCE",
+  apiKey: "API-KEY",
   authDomain: "mymoney-75b90.firebaseapp.com",
   projectId: "mymoney-75b90",
   storageBucket: "mymoney-75b90.firebasestorage.app",
   messagingSenderId: "882717967006",
-  appId: "1:882717967006:web:6bd9727bb115a36654718f"
+  appId: "API-ID"
 };
 
 const app = initializeApp(firebaseConfig);
